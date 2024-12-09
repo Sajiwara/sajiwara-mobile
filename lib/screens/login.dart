f607b765c081ebca87033c7fb50f64fb299afa34
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2024-12-09 12:20:13 +0700

    tampilan untuk review

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -45,7 +45,7 @@ class _LoginPageState extends State<LoginPage>
     String password = _passwordController.text;
 
     final response = await request.login(
-        "http://theresia-tarianingsih-sajiwaraweb.pbp.cs.ui.ac.id/auth/login/",
+        "http://127.0.0.1:8000/auth/login/",
         {
           'username': username,
           'password': password,
